eb8ec4e0a654bd9a445cdd208e2c35347e945832
Nguyen Khac Tuan <ktuan439@gmail.com>
2023-05-14 13:31:25 +0700

    add log error in middleware

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -7,8 +7,9 @@ const notFound = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     const message = err.message || 'Something went wrong';
+    console.log(err);
     if (statusCode == 500) {
-        console.log(err);
+        // console.log(err);
         res.status(500).json({
             message: 'Xảy ra lỗi khi xử lý phía máy chủ',
             stack: process.env.NODE_ENV === 'production' ? null : err.stack,
